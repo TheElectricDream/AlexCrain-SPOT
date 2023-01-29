43d42ac5676726df6c9a6d63c3afb33202b8502a
Alexander Crain <68435427+TheElectricDream@users.noreply.github.com>
2023-01-29 10:33:26 -0500

    Cleaning Up
    - Cleared dev folder
    - Added the weight import to the useful code base

--- Development/depp_net_model/conver_to_h5.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras.models import save_model, Sequential
-
-model = tf.keras.models.load_model("./export_dir/0", compile=False)
-
-tf.model.save(model,"./export_dir/0/new_model.h5", save_format='h5')
