f5a9a2515101c25eb1dc35cd42dec38e036cc980
Kirk Hovell <68435427+TheElectricDream@users.noreply.github.com>
2023-04-05 14:30:48 -0400

    Post-Obruta Upload

--- Projects/Obruta_Experiment/Batch_Scripts/CustomStopModel.bat
+++ Projects/Obruta_Experiment/Batch_Scripts/CustomStopModel.bat
@@ -1,1 +1,1 @@
-plink.exe -ssh pi@192.168.0.100 -pw raspberry command "nohup sudo killall ./ExperimentSoftware/FloatPlatform.elf >/dev/null 2>&1 &"
+plink.exe -ssh pi@192.168.0.102 -pw raspberry command "nohup sudo killall ./ExperimentSoftware/CleanShutdown.elf >/dev/null 2>&1 &"
